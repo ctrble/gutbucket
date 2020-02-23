b925f2940cabf3df63bf5be6f065ea2ac112f1f2
Charlotte Trible <ctrble@gmail.com>
2020-02-23 12:50:27 -0600

    adding some basic controllers

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+  public static GameController instance = null;
+
+  public GameObject playerPrefab;
+
+  void Awake() {
+    CreateSingleton();
+  }
+
+  void CreateSingleton() {
+    if (instance == null)
+      instance = this;
+    else if (instance != this)
+      Destroy(gameObject);
+
+    DontDestroyOnLoad(gameObject);
+  }
+
+  void OnEnable() {
+    GameObject player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+  }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class InputController : MonoBehaviour {
+  public int playerId = 0; // The Rewired player id of this character
+  private Player player; // The Rewired Player
+  private float deadZone = 0.071f;
+
+  void Awake() {
+    player = ReInput.players.GetPlayer(playerId);
+  }
+
+  public bool Accelerate() {
+    return player.GetButton("Accelerate");
+  }
+
+  public bool AccelerateStart() {
+    return player.GetButtonDown("Accelerate");
+  }
+
+  public bool Brake() {
+    return player.GetButton("Brake");
+  }
+
+  public bool BoostStart() {
+    return player.GetButtonDown("Boost");
+  }
+
+  public bool ReverseStart() {
+    return player.GetButtonDown("Reverse");
+  }
+
+  public Vector3 PlayerMove() {
+    Vector2 stickInput = new Vector2(player.GetAxis("Move_Horizontal"), player.GetAxis("Move_Vertical"));
+    if (stickInput.sqrMagnitude < deadZone * deadZone) {
+      stickInput = Vector2.zero;
+    }
+
+    return new Vector3(player.GetAxis("Move_Horizontal"), 0, player.GetAxis("Move_Vertical"));
+  }
+
+  public bool Shoot() {
+    return player.GetButton("Shoot");
+  }
+
+  public Vector3 AimVector() {
+    Vector2 stickInput = new Vector2(player.GetAxis("Aim_Horizontal"), player.GetAxis("Aim_Vertical"));
+    if (stickInput.sqrMagnitude < deadZone * deadZone) {
+      stickInput = Vector2.zero;
+    }
+
+    return new Vector3(stickInput.x, 0, stickInput.y);
+  }
+
+  public Vector3 AimDelta() {
+    return new Vector3(player.GetAxisDelta("Aim_Horizontal"), 0, player.GetAxisDelta("Aim_Vertical"));
+  }
+}
