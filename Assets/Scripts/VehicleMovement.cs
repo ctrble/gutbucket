9bad985190a2326a59d4e70f9aa01e1a01d8359c
Charlotte Trible <ctrble@gmail.com>
2020-02-23 13:55:21 -0600

    starting to set up wheels

--- Assets/Scripts/VehicleMovement.cs
+++ Assets/Scripts/VehicleMovement.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VehicleMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class VehicleMovement : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public enum MovementState { Airborne, Tilt, Idle, Drive, Brake, Skid };
+  public MovementState currentState;
+  public AirborneVehicle airborneState;
+  public IdleVehicle idleState;
+  public DriveVehicle driveState;
+  public BrakeVehicle brakeState;
+  public SkidVehicle skidState;
+  public TiltVehicle tiltState;
+
+  [Space]
+  [Header("Helpers")]
+
+  public bool boost;
+  public bool reverse;
+  public bool grounded;
+  public int groundedWheels;
+  public float currentSpeed;
+  private float tempSpeed;
+  private float lastSpeed;
+  public float gravityForce;
+  public float currentAngle;
+  public float currentZAngle;
+
+  [Space]
+  [Header("Components")]
+
+  public VehicleData vehicleData;
+  public InputController playerInput;
+  // public Wheel_Controller[] wheelControllers;
+  [SerializeField]
+  public Rigidbody vehicleRB;
 }
---
+++ Assets/Scripts/VehicleWheel.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleWheel : MonoBehaviour {
+
+  [Header("Components")]
+
+  private VehicleData vehicleData;
+  private VehicleMovement vehicleMovement;
+  private Rigidbody vehicleRB;
+  private CapsuleCollider vehicleCollider;
+  public GameObject wheelPrefab;
+  public GameObject wheelChild;
+
+  [Space]
+  [Header("Data")]
+
+  public bool grounded;
+  public Vector3 groundNormal = Vector3.up;
+  private Vector3 wheelForce = Vector3.zero;
+  public RaycastHit[] wheelHits = new RaycastHit[1];
+
+  [Space]
+  [Header("Settings")]
+
+  public bool steering = false;
+  private bool wheelBelowCenter;
+  public float skinWidth;
+  public float wheelRadius;
+  private float hoverHeight;
+  private float hoverForce;
+  private Vector3 wheelRestOffset;
+  private Vector3 wheelGroundedOffset;
+  private Vector3 wheelMaxOffset;
+  private float distancePercent;
+
+  void Start() {
+    if (vehicleRB == null) {
+      vehicleRB = gameObject.GetComponentInParent<Rigidbody>();
+    }
+    if (vehicleCollider == null) {
+      vehicleCollider = transform.root.GetComponentInChildren<CapsuleCollider>();
+    }
+    if (vehicleData == null) {
+      vehicleData = gameObject.GetComponentInParent<VehicleData>();
+    }
+    if (vehicleMovement == null) {
+      vehicleMovement = gameObject.GetComponentInParent<VehicleMovement>();
+    }
+
+    SetUpWheels();
+  }
+
+  void SetUpWheels() {
+    // Set the height of the wheel
+    skinWidth = vehicleCollider.radius * (vehicleCollider.radius * 0.2f);
+    Vector3 local = transform.localPosition;
+    local.y = vehicleCollider.radius - skinWidth;
+    transform.localPosition = local;
+
+    // Physics Init
+    hoverForce = vehicleMovement.gravityForce;
+    hoverHeight = (vehicleCollider.radius * 2) + skinWidth;
+
+    // Wheel Init
+    grounded = false;
+
+    // Position the wheel
+    Quaternion wheelRotation = Quaternion.LookRotation(transform.forward, -transform.right);
+
+    wheelRadius = wheelPrefab.GetComponent<CapsuleCollider>().radius;
+    wheelRestOffset = -transform.up * (hoverHeight - wheelRadius);
+    wheelGroundedOffset = wheelRestOffset;
+    wheelMaxOffset = -transform.up * (hoverHeight - (wheelRadius * 2));
+
+    wheelChild = Instantiate(wheelPrefab, transform.position + wheelRestOffset, wheelRotation, transform);
+  }
+}
